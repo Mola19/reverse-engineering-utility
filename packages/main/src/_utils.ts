5f9a7c452808c345f6c40276f0853e1585293b46
may <m4rch3n1ng@gmail.com>
2023-04-22 23:20:31 +0200

    read dependencies and create package.json

--- packages/main/src/_utils.ts
+++ packages/main/src/_utils.ts
@@ -1,4 +1,4 @@
-import { mkdir, readdir } from "node:fs/promises"
+import { mkdir, readFile, readdir, writeFile } from "node:fs/promises"
 import { join as joinPath } from "node:path"
 import { app } from "./index.js"
 import { existsSync } from "node:fs"
@@ -20,6 +20,25 @@ export async function waitNpm () {
 
 export async function npmInstall () {
 	const protocolDir = getUserData("matrix-protocols")
+
+	const files = await readdir(protocolDir)
+	const jsFiles = files.filter(( file ) => file.endsWith(".js"))
+	const jsonFiles = jsFiles.map(( jsFile ) => joinPath(protocolDir, jsFile.replace(/.js$/, ".json")))
+
+	const content = await Promise.all(jsonFiles.map(( jsonFile ) => existsSync(jsonFile) && readFile(jsonFile)))
+	const json = content.filter(( file ) => file).map(( file ) => JSON.parse(file.toString()))
+	const deps = json.map(({ dependencies }) => dependencies || []).flat()
+	const dependencies = Object.fromEntries(deps.map(( dep ) => [ dep, "latest" ]))
+
+	const pkg = {
+		name: "reverse-engineering-matrix-protocols",
+		dependencies: dependencies,
+		type: "commonjs",
+		private: true
+	}
+
+	await writeFile(joinPath(protocolDir, "package.json"), JSON.stringify(pkg, null, "\t"))
+
 	process.argv = [ process.argv[0]!, "npm", "install", "--prefix", protocolDir ]
 
 	// @ts-expect-error
